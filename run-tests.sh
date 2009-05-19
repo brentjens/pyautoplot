1e82f44a2d11dc81104a0e934d060540ce53ad35
Michiel Brentjens <brentjens@astron.nl>
2009-05-19 15:37:35 +0200

    Improved unit testing script.

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
+logfile=testlog.txt
+
+rm  $logfile
+
 modules="angle pyautoplot"
 for module in $modules; do
-    echo
-    echo "*** Testing module ${module} ***"
-    echo
-    python test${module}.py
+    echo >> $logfile
+    echo "*** Testing module ${module} ***">>$logfile 
+    echo >> $logfile
+    python test${module}.py 2>>$logfile
     echo Completed testing  ${module}
-    echo
-    echo ================================================================================ 
+    echo ================================================================================ >> $logfile
 done
+
+echo
+echo
+echo S U M M A R Y
+echo
+cat testlog.txt |grep -e 'Ran\|OK\|FAILED\|Testing'
