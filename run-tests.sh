1bc0e339be38218844531b929bab8241d029223e
Michiel Brentjens <brentjens@astron.nl>
2009-10-08 08:56:15 +0200

    Added figleaf code coverage support.

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 logfile=testlog.txt
 
 rm  $logfile
+rm .figleaf
+
+FIGLEAF=`which figleaf`
+
+if test "$FIGLEAF" = ""; then
+    echo "Cannot find figleaf. Proceeding without test coverage analysis"
+fi
+
 if touch $logfile; then
     echo "Will write results to $logfile"
 else
@@ -10,15 +18,19 @@ else
     logfile=/dev/null
 fi
 
-
 modules=`ls *.py|grep -v "^test"`
 for module in $modules; do
-    echo >> $logfile
-    echo "*** Testing module ${module} ***">>$logfile 
-    echo >> $logfile
+    echo |tee -a $logfile
+    echo "*** Testing module ${module} ***"|tee -a $logfile 
+    echo |tee -a $logfile
     testfile=test${module}
     if test -e $testfile; then
-        python $testfile 2>>$logfile
+        
+        if test "$FIGLEAF" = ""; then
+            python $testfile 2>&1 |tee -a $logfile |grep -e 'Ran\|OK\|FAILED\|Testing\|Error\|^    \|^  File \^\|line\|^Traceback\| != \| !< '
+        else
+            figleaf $testfile 2>&1 |tee -a $logfile |grep -e 'Ran\|OK\|FAILED\|Testing\|Error\|^    \|^  File \^\|line\|^Traceback\| != \| !< '
+        fi
         echo Completed testing  ${module}
     else
         echo "Error: Module $module has no associated test" |tee -a $logfile
@@ -26,11 +38,15 @@ for module in $modules; do
     echo ================================================================================ >> $logfile
 done
 
+if test "$FIGLEAF" != ""; then
+    figleaf2html -d coverage/ .figleaf
+fi
+
 
 if test "$logfile" = "testlog.txt"; then
     echo
     echo
     echo S U M M A R Y
     echo
-    cat testlog.txt |grep -e 'Ran\|OK\|FAILED\|Testing\|Error\|^    \|^  File \^\|line\|^Traceback'
+    cat $logfile  |grep -e 'Ran\|OK\|FAILED\|Testing\|Error\|^    \|^  File \^\|line\|^Traceback\| != \| !< '
 fi
