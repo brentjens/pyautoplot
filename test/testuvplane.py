7143c665430a8c6947e92887569d9835203498d1
Michiel Brentjens <brentjens@astron.nl>
2010-10-21 16:07:23 +0200

    unittest stub for uvplane.py

---
+++ test/testuvplane.py
@@ -0,0 +1,33 @@
+import unittest
+from pyautoplot.uvplane import *
+
+
+
+class UvplaneTest(unittest.TestCase):
+    def test_quadrant(self):
+        pass
+
+    def test_fixup_rgb(self):
+        pass
+
+    def test_color_from_angle(self):
+        pass
+
+    def test_rgb_scale_palette(self):
+        pass
+
+    def test_phase_palette(self):
+        pass
+
+    def test_rgb_from_complex_image(self):
+        pass
+
+    def test_read_fits_image(self):
+        pass
+        
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+    pass
