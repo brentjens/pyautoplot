0e5eaf968fe9cb05ebfccf37f20a079c18dccc29
Michiel Brentjens <brentjens@astron.nl>
2010-10-21 21:17:30 +0200

    Added test case for uvplane.color_from_angle

--- test/testuvplane.py
+++ test/testuvplane.py
@@ -41,9 +41,33 @@ class UvplaneTest(unittest.TestCase):
         pass
 
     def test_color_from_angle(self):
+        rgb_pr = color_from_angle(0.0)
+        rgb_pi = color_from_angle(pi/2)
+        rgb_nr = color_from_angle(pi)
+        rgb_ni = color_from_angle(pi*1.5)
+
+        self.assertEquals(len(rgb_pr.shape), 1)
+        self.assertEquals(rgb_pr.shape[0], 3)
+
+        self.assertAlmostEquals(rgb_pr[0], 1.0)
+        self.assertAlmostEquals(rgb_pr[1], 0.0)
+        self.assertAlmostEquals(rgb_pr[2], 0.0)
+        
+        self.assertAlmostEquals(rgb_pi[0], 0.0)
+        self.assertAlmostEquals(rgb_pi[1], 1.0)
+        self.assertAlmostEquals(rgb_pi[2], 0.0)
+
+        self.assertAlmostEquals(rgb_nr[0], 1.0)
+        self.assertAlmostEquals(rgb_nr[1], 0.0)
+        self.assertAlmostEquals(rgb_nr[2], 0.75)
+
+        self.assertAlmostEquals(rgb_ni[0], 0.0)
+        self.assertAlmostEquals(rgb_ni[1], 0.75)
+        self.assertAlmostEquals(rgb_ni[2], 0.75)
         pass
 
     def test_rgb_scale_palette(self):
+        
         pass
 
     def test_phase_palette(self):
