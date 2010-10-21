daa6730e1d484ff8b39db2cc2a5cbe6717600e78
Michiel Brentjens <brentjens@astron.nl>
2010-10-21 16:23:30 +0200

    Added test for uvplane.fixup_rgb

--- test/testuvplane.py
+++ test/testuvplane.py
@@ -14,6 +14,30 @@ class UvplaneTest(unittest.TestCase):
         pass
 
     def test_fixup_rgb(self):
+        a = array([[0.99, 1.0, 1.001],[0.001, 0.0, -0.001]])
+        f = fixup_rgb(a)
+
+        empty=fixup_rgb(array([]))
+        self.assertEquals(empty.shape, (0,))
+        
+        self.assertEquals(len(a.shape), 2)
+        self.assertEquals(len(f.shape), 2)
+        self.assertEquals(a.shape[0], f.shape[0])
+        self.assertEquals(a.shape[1], f.shape[1])
+
+        self.assertAlmostEquals(a[0,0], 0.99)
+        self.assertAlmostEquals(a[0,1], 1.0)
+        self.assertAlmostEquals(a[0,2], 1.001)
+        self.assertAlmostEquals(a[1,0], 0.001)
+        self.assertAlmostEquals(a[1,1], 0.0)
+        self.assertAlmostEquals(a[1,2], -0.001)
+        
+        self.assertAlmostEquals(f[0,0], 0.99)
+        self.assertAlmostEquals(f[0,1], 1.0)
+        self.assertAlmostEquals(f[0,2], 1.0)
+        self.assertAlmostEquals(f[1,0], 0.001)
+        self.assertAlmostEquals(f[1,1], 0.0)
+        self.assertAlmostEquals(f[1,2], 0.0)
         pass
 
     def test_color_from_angle(self):
