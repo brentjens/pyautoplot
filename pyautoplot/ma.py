8907f8eeb4e5f3d90d5ff4547f778e693821d462
Michiel Brentjens <brentjens@astron.nl>
2010-10-25 21:14:47 +0200

    Make exception handling python 2.5 compatible

--- pyautoplot/ma.py
+++ pyautoplot/ma.py
@@ -1,4 +1,8 @@
 try:
     from numpy.ma import *
-except (ImportError, ) as e:
+except (ImportError, ):    # Use this construction to be backwards
+    e = sys.exc_info()[1]  # compatible with Python 2.5.  Proper
+                           # Python 2.6/2.7/3.0 is
+                           # except ValueError as e:
+                           # etc...
     from numpy.core.ma import *
