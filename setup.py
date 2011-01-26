2b37c3403647314cc47d16c67b1b18f981663a05
Michiel Brentjens <brentjens@astron.nl>
2011-01-26 16:19:12 +0100

    Started development of release v0.3

--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from distutils.core import setup
 
 setup(name='pyautoplot',
-      version='0.2',
+      version='0.3',
       description='Library to build interferometric data inspection tools',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
       url='',
       packages=['pyautoplot'],
-      scripts=['scripts/msinspect'],
+      scripts=['scripts/msplots', 'scripts/tscount',
+               'scripts/launch-msplots.sh',
+               'scripts/launch-tscount.sh'],
      )
