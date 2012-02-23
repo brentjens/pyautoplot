290b151c0d0b08ef44db09d84885ae98ec41404b
Michiel Brentjens <brentjens@astron.nl>
2012-02-23 16:19:19 +0100

    Begin v 0.7 development (again)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyautoplot',
-      version='0.6.1',
+      version='0.7',
       description='Library to build interferometric data inspection tools',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
