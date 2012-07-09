705ab74d4fd52a9063749df150872db90b8bb723
Michiel Brentjens <brentjens@astron.nl>
2012-07-09 14:24:41 +0200

    Improved logging / version 0.9.3

--- scripts/launch-msplots.sh
+++ scripts/launch-msplots.sh
@@ -8,6 +8,7 @@
 
 HOSTNAME=`hostname -s`
 PATH="$PATH:/opt/cep/pyautoplot/bin"
+LOG=/globaldata/inspect/launch-msplots.log
 
 if test "$HOSTNAME" == "lhn001"; then
     cexec locus: "bash -ilc \"use LofIm; use Pythonlibs; use Pyautoplot; msplots $@\""
@@ -16,17 +17,20 @@ if test "$HOSTNAME" == "lhn001"; then
     date >> /globaldata/inspect/launch-msplots.log
     echo "$@" >> /globaldata/inspect/launch-msplots.log
     if test "$CREATE_HTML" == ""; then
-        echo "Cannot find create_html: no HTML generated" | tee -a /globaldata/inspect/launch-msplots.log
+        echo "Cannot find create_html: no HTML generated" | tee -a $LOG
 
     else
-        echo "Creating HTML using $CREATE_HTML" | tee -a /globaldata/inspect/launch-msplots.log
-        result=`bash -ilc \"$CREATE_HTML $@ 2>&1\"`
+        echo "Creating HTML using $CREATE_HTML" | tee -a $LOG
+        command="$CREATE_HTML $@ 2>&1"
+        echo "$command"| tee -a $LOG
+        result=`$command`
         exit_status="$?"
         if test "$exit_status" == "0"; then
-            echo "HTML Created successfully" | tee -a /globaldata/inspect/launch-msplots.log
+            echo "HTML Created successfully" | tee -a $LOG
         else 
-            echo "Problem creating HTML overview for $@." | tee -a /globaldata/inspect/launch-msplots.log
-            echo "$result" | tee -a /globaldata/inspect/launch-msplots.log
+            echo "Problem creating HTML overview for $@." | tee -a $LOG
+            echo "Exit status: $exit_status" | tee -a $LOG
+            echo "$result" | tee -a $LOG
         fi
     fi
     echo "Done"
