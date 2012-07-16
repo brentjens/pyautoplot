4533a4ecbaa69e62bcc6c54348960fab7034d4d8
Michiel Brentjens <brentjens@astron.nl>
2012-07-16 12:15:49 +0200

    pyautoplot 0.9.6

--- pyautoplot/__init__.py
+++ pyautoplot/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.9.5'
+__version__ = '0.9.6'
 
 from pyautoplot.main import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pyautoplot',
-      version='0.9.5',
+      version='0.9.6',
       description='Library to build interferometric data inspection tools',
       author='Michiel Brentjens',
       author_email='brentjens@astron.nl',
