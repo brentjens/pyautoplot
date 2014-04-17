1ec0ff6fadc70acc347d7bfbe7777444f646ee4e
Michiel Brentjens <brentjens@astron.nl>
2014-04-17 14:40:03 +0200

    Make rsync forget about setting the group, which would not work between systems.

--- cep-install.sh
+++ cep-install.sh
@@ -6,5 +6,5 @@ function lfc {
     ssh -AYCtt  ${USER}@portal.lofar.eu  "ssh -AYCtt  $1 'bash -ilc \"$2\"'"
 }
 
-rsync -avz --delete -e "ssh -A ${USER}@portal.lofar.eu ssh" ./ ${USER}@lhn001:pyautoplot/
+rsync -a --no-group -vz --delete -e "ssh -A ${USER}@portal.lofar.eu ssh" ./ ${USER}@lhn001:pyautoplot/
 
