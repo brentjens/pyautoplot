644c39920d78313f2b0138ed0bfe1cce6f213476
Ruud Beukema <ruudbeukema@gmail.com>
2016-10-12 15:05:13 +0200

    Finalized statistics presentation in tooltip and changed data product completion threshold from 0.01 to 0.005 after discussion with ScS and applied it to f0seqnr-based calculation as well.

--- test/testf0seqnrcalculation.py
+++ test/testf0seqnrcalculation.py
@@ -14,12 +14,19 @@ sys.path.append("../scripts")
 import create_html
 
 
-
-
 def test_main(in_sas_id, in_parset_path, in_file_sizes_path, in_f0seqnr_sizes_path):
     result = True
     
     parset = create_html.parset_summary(in_sas_id, in_parset_path)
+    
+    file_sizes_dict = create_html.parse_file_sizes(in_file_sizes_path)
+    analysed_file_sizes_dict  = create_html.file_size_analysis(parset, file_sizes_dict)
+    highest_file_size_mb = analysed_file_sizes_dict['max_ms_size_mb'].max()
+    
+    f0seqnr_sizes_dict = create_html.parse_file_sizes(in_f0seqnr_sizes_path, os.path.dirname)
+    f0seqnr_completeness_dict = create_html.f0seqnr_size_analysis(parset, f0seqnr_sizes_dict)
+    f0seqnr_completeness_statistics = create_html.calculate_statistics(f0seqnr_completeness_dict.values(), (100.0, None))
+    
     print("Relevant parset details:")
     print("clock_mhz: ", parset['clock_mhz'])
     print("start-time: ", parset['start_time'])
@@ -32,49 +39,64 @@ def test_main(in_sas_id, in_parset_path, in_file_sizes_path, in_f0seqnr_sizes_pa
     print("Beamformer locations: ", "\n".join(parset['beamformer_locations']))
     
     
-    print("\nContent of [file_sizes_dict]:")
-    file_sizes_dict = create_html.parse_file_sizes(in_file_sizes_path)
+    print("\nContent of [file_sizes_dict]:")    
     for data_product_folder, (_, _, file_size_in_mb) in file_sizes_dict.items():    
         print(data_product_folder, " (", file_size_in_mb, "MB)")       
-    
+
     
     print("\nContent of [analysed_file_sizes_dict]:")
-    analysed_file_sizes_dict  = create_html.file_size_analysis(parset, file_sizes_dict)
-    highest_file_size_mb = analysed_file_sizes_dict['max_ms_size_mb'].max()
     print("max_ms_size_mb: ", analysed_file_sizes_dict['max_ms_size_mb'])
     print("max_raw_size_mb: ", analysed_file_sizes_dict['max_raw_size_mb'])
     print("missing_data_sets: ", analysed_file_sizes_dict['missing_data_sets'])
     print("odd_sized_data_sets: ", analysed_file_sizes_dict['odd_sized_data_sets'])
     print("percentage_complete: ", analysed_file_sizes_dict['percentage_complete']) 
+        
     
     print("\nContent of [f0seqnr_sizes_dict]:")
-    f0seqnr_sizes_dict = create_html.parse_file_sizes(in_f0seqnr_sizes_path, os.path.dirname)
     for data_product_folder, (_, _, nr_integration_periods_in_file) in f0seqnr_sizes_dict.items():        
         print(data_product_folder, " (", nr_integration_periods_in_file, ")")        
-    
-    print("\nContent of [completeness_dict]:")
-    completeness_dict = create_html.f0seqnr_size_analysis(parset, f0seqnr_sizes_dict)
-    for data_product_folder, completeness_value in completeness_dict.items():
+
+
+    print("\nContent of [f0seqnr_completeness_dict]:")
+    for data_product_folder, completeness_value in f0seqnr_completeness_dict.items():
         print(data_product_folder, " (", completeness_value, ")")
         
-    print("\nTotal average completeness: ", create_html.calculate_average(completeness_dict.values()))
+    print("\nTotal average completeness: ", f0seqnr_completeness_statistics, " over ", len(f0seqnr_completeness_dict.values()), " number of items")
     
     print("\nIncomplete datasets according to original method (odd_sized_data_sets (=) abs(float(data_size_mb)/float(max_ms_size_mb) -1.0) > 0.01):")
     for (name, size) in sorted(analysed_file_sizes_dict['odd_sized_data_sets']):
         print("Dataset: ", name, " Size: ", size, "MB")
     
     print("\nIncomplete datasets according to f0seqnr method (completeness_value < 100):")
-    for data_product_folder, completeness_value in completeness_dict.items():
-        if completeness_value < 100:
+    for data_product_folder, completeness_value in f0seqnr_completeness_dict.items():
+        if completeness_value < 99.95:
             print("Dataset: ", data_product_folder, " Completeness: %0.1f%%" % completeness_value)
             
-    print("\nIncomplete datasets based on relative (%rMB) file size:" % highest_file_size_mb)
+    print("\nIncomplete datasets based on relative (Max size = %rMB) file size:" % highest_file_size_mb)
     for data_product_folder, (_, _, file_size_in_mb) in file_sizes_dict.items():  
         if file_size_in_mb < highest_file_size_mb:
-            print("Dataset: ", data_product_folder, " Size: ", file_size_in_mb, "MB ", "(%0.1f%%)" % (100*file_size_in_mb/highest_file_size_mb))
+            print("Dataset: ", data_product_folder, " Size: ", file_size_in_mb, "MB ", "(%0.f%%)" % (100*file_size_in_mb/highest_file_size_mb))
             
             
-    print('\n'.join(['%s: %dMB (%0.f%%)' % (name, size, completeness_dict.get(name, -1)) for (name, size) in sorted(analysed_file_sizes_dict['odd_sized_data_sets'])]))
+    print('\n'.join(['%s: %dMB (%0.f%%)' % (name, size, f0seqnr_completeness_dict.get(name, -1)) for (name, size) in sorted(analysed_file_sizes_dict['odd_sized_data_sets'])]))
+    
+    open("./index.html", 'w').write('''
+    <html>
+    <head>
+        <meta http-equiv="refresh" content="60">
+        <title>LOFAR Inspection plots</title>
+    </head>
+    <body>
+        <h1>LOFAR inspection plots</h1>
+        <table>
+        <tr><th>SAS ID</th> <th>Campaign</th> <th>Target</th> <th>DynSpec</th> <th title="Percentage of odd sized data products per project\n\nWhere 'odd sized' is defined as:\nData products with less than %0.2f%% completeness">Compl</th> <th title="Average completeness percentage of odd sized data products (based on f0seqnr sizes)\n\nWhere 'odd sized' is defined as:\nData products with less than %0.2f%% completeness">Compl*</th> <th>AntennaSet</th> <th>Band</th> <th>Start</th> <th>End</th> <th>Clock</th> <th>Subb</th> <th>Parset</th></tr>
+        %s
+        </table>
+    </body>
+</html>
+    ''' %   (100*(1-create_html.DATA_INCOMPLETE_THRESHOLD),
+            100*(1-create_html.DATA_INCOMPLETE_THRESHOLD),
+            create_html.observation_table_row(parset, analysed_file_sizes_dict, f0seqnr_completeness_dict, "./")))
     
     return result
     
